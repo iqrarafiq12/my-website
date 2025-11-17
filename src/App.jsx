9b5168b7cbad6a5be9445ec024f9e8e4a33862e4
Iqra Rafiq <iqrak0128@gmail.com>
2025-11-18 04:44:43 +0500

    Hero Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import HeroSection from './Sections/Hero/HeroSection'
 
 function App() {
   return (
     <main>
       <Navbar />
+      <HeroSection />
     </main>
   )
 }
