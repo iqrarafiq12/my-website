bf930314dd98b080d07b43ca9887577c9b858607
Iqra Rafiq <iqrak0128@gmail.com>
2025-11-18 14:53:15 +0500

    Scroll Trigger + Hero Section ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import HeroSection from './Sections/Hero/HeroSection'
+import { ScrollTrigger } from 'gsap/all';
+import gsap from 'gsap';
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <div className="h-dvh border border-red-500" />
     </main>
   )
 }
